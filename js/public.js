0565611996f1bb9c98fb8310862b1f2e6f80c65e
SaSaChu <teresa7397@gmail.com>
2018-08-15 17:32:05 +0800

    Fix html code.

--- js/public.js
+++ js/public.js
@@ -1,10 +1,35 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 - 2018, OAF2E
- * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://www.ioa.tw/
- */
- 
-$(function () {
+
+
+$(function(){
+
+  $('button').click(function(){
+
+    var v = $('.in1').val(); // 元素？字串？
+
+    var $div = $('<div />').addClass('rbox');
+    var $p = $('<p />').addClass('se').text(v);
+    var $a = $('<a />').text('Detel');
+
+
+    $('.rboxs')
+      .append($div
+        .append($p).append($a)
+      );
+
+    $p.click(function() {
+
+      var a = prompt("請輸入你想說的");
+
+      $p.text(a);
+
+    });
+
+    $a.click(function() {
+      $div.remove();
+    });
+
+    $('#in').val('');
+
+  });
 
 });
