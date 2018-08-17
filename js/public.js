6e5d1b54d3d5881d8379706a94e049b8ccde7786
SaSaChu <teresa7397@gmail.com>
2018-08-17 18:33:58 +0800

    Fix html code.

--- js/public.js
+++ js/public.js
@@ -5,6 +5,31 @@
  * @link        https://www.ioa.tw/
  */
  
+
+ // console.log('文字信息');
+
 $(function () {
+
+  $('button').click(function() {
+
+    var a = $('.a1').val();
+    var b = $('.a2').val();
+    
+    if(a > b) {
+      $('.a3').text(a +'大於'+ b);
+    } else if (a < b) {
+      $('.a3').text(a +'小於'+ b);
+    } else if (a == b) {
+      $('.a3').text(a +'等於'+ b);
+    }
+
+    if(a != 'a') {
+      $('.a3').text('第1格輸入框，請輸入數字');
+    } else if(b != 'b') {
+      $('.a3').text('第2格輸入框，請輸入數字');
+    }
+
+
+  });
 
 });
