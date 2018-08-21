580e0106e4d37bfab211043e57524f6186509ea8
SaSaChu <teresa7397@gmail.com>
2018-08-21 22:48:55 +0800

    Fix html code.

--- js/public.js
+++ js/public.js
@@ -14,21 +14,16 @@ $(function () {
 
     var a = $('.a1').val();
     var b = $('.a2').val();
-    
-    if(a > b) {
+
+    if(isNaN(a) || isNaN(b)) {
+      $('.a3').text('請輸入數字');
+    } else if(a > b) {
       $('.a3').text(a +'大於'+ b);
     } else if (a < b) {
       $('.a3').text(a +'小於'+ b);
     } else if (a == b) {
       $('.a3').text(a +'等於'+ b);
     }
-
-    if(a != 'a') {
-      $('.a3').text('第1格輸入框，請輸入數字');
-    } else if(b != 'b') {
-      $('.a3').text('第2格輸入框，請輸入數字');
-    }
-
 
   });
 
